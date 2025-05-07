1456b4085f24d443557344f13df1b14a61d2d368
@sarujan1 <sarujan@codelantic.com>
2025-05-07 11:01:47 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './component/navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/home'
 
 const App = () => {
 
@@ -9,6 +10,12 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath &&  <Navbar />}
+      
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
